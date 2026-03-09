77cbb7924839bca6e521c4f5423cb9cb8e3d282b
surumi K A <surumika44@gmail.com>
2026-03-09 21:02:11 +0530

    collection and product

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,24 +3,36 @@ import {assets} from '../assets/assets'
 function Footer() {
   return (
     <>
-      <div className='flex  gap-20 text-sm my-6'>
-        <div>
+      <div className='flex justify-around  text-sm   p-20 bg-red-100'>
+        <div className='flex-1'>
             <img src={assets.logo} className='w-32 m-5'/>
-            <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Harum rem voluptates </p>
+            <p className='w-20 md:w-2/3 text-gray-600 '>Lorem ipsum dolor sit amet consectetur 
+                adipisicing. </p>
         </div>
-        <div >
-            <p className='text-xl font-medium mb-5'>COMPANY</p>
+        <div className='flex-1'>
+            <p className='text-xl font-medium mb-5 '>COMPANY</p>
             <ul className='flex flex-col gap-1 text-gray-600 text-sm '>
                 <li>Home</li>
                 <li>About</li>
                 <li>Delivery</li>
                 <li>Privacy Policy</li>
             </ul>
-
-        </div>
+           </div>  
+            <div className='flex-1'>
+              <p className='text-xl font-medium mb-5'>GET IN TOUCH </p>
+              <ul className='flex flex-col gap-1 text-gray-600 text-sm '>
+                <li>0484-789-456-123</li>
+                <li>contactatforever.com</li>
+                
+            </ul>
+            </div>
+       
         
         </div>  
+        <div className='mt-10'>
+          <hr className='border-gray-400'></hr>
+          <p className='py-5 text-sm text-center text-gray-600 mt-5'>Copyright2024@forever.com-All Right Reserved</p>
+        </div>
     </>
   )
 }
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { shopcontext } from '../context/shopcontext'
+import { useState } from 'react';
+import Productitem from '../components/productitem';
 
 function Collection() {
+  const{products}=useContext(shopcontext);
+  const[filterproduct,setfilterproduct]=useState([]);
+  const[category,setcategory]=useState([]);
+  const[subcategory,setsubcategory]=useState([]);
+
+  const togglecategory = (e) =>{
+    if(category.includes(e.taget.value)){
+        setcategory(prev=>prev.filter(item => item != e.target.value))
+    }
+    else{
+      setcategory([...prev,e.target.value])
+    }
+  }
+  const togglesubcategory = (e) =>{
+    if(subcategory.includes(e.taget.value)){
+        setsubcategory(prev=>prev.filter(item => item != e.target.value))
+    }
+    else{
+      setsubcategory([...prev,e.target.value])
+    }
+  }
+
+  useEffect(()=>{
+    setfilterproduct(products);
+  },[])
+
+
+
+
   return (
-    <div></div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-5 mx-10 md:mx-28 items-start mt-9'> 
+      
+      
+      <div className='min-w-50'>
+        <p className='text-lg text-gray-600 font-medium '>FILTER</p>
+        <div className='border border-gray-300 p-5 w-60 flex flex-col my-3'>
+          <div className='gap-1 text-gray-600 mb-2'>CATEGORIES</div>
+          <label className='flex gap-2 text-sm'><input type='checkbox' value="men" onChange={togglecategory} />Men</label>
+          <label className='flex gap-2 text-sm'><input type='checkbox' value="women" onChange={togglecategory} />Women</label>
+          <label className='flex gap-2 text-sm'><input type='checkbox'value='kids' onChange={togglecategory} />Kids</label>
+        </div>
+         
+        <div className='border border-gray-300 p-5 w-60 flex flex-col my-3'>
+          <div className='gap-1 text-gray-600 mb-2'>TYPE</div>
+          <label className='flex gap-2 text-sm'><input type='checkbox' value='topwear' onChange={togglesubcategory} />Topwear</label>
+          <label className='flex gap-2 text-sm'><input type='checkbox' value='bottomwear' onChange={togglesubcategory} />Bottomwear</label>
+          <label className='flex gap-2 text-sm'><input type='checkbox' value=' winterwear'onChange={togglesubcategory} />Winterwear</label>
+        </div>
+      </div>
+
+      
+      <div className='flex-1'>
+        <div className='flex justify-between items-center mb-6 '>
+          <p className='  text-3xl  text-gray-500'>ALL <span className='text-3xl text-gray-700' >COLLECTION</span> </p>
+          
+          <select className='border border-gray-400 text-sm px-3 py-2'>
+            <option>Sort by: RELEVANT</option>
+            <option>Sort by: HIGH</option>
+            <option>Sort by: LOW</option>
+          </select>
+        </div>
+        <div className='grid grid-cols-4 gap-4 gap-y-6'>
+          {
+            filterproduct.map((item,index)=>(
+              <Productitem  key={index} id={item._id} image={item.image} name={item.name} price={item.price}   />
+            ))
+          }
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
