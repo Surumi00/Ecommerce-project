5063a08542b81d2a8d62f0e670ec79d47b190389
surumi K A <surumika44@gmail.com>
2026-04-06 20:06:44 +0530

    login page ready

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,8 +31,8 @@ function Navbar() {
         </ul>
         <div className='flex items-center gap-5'>
           <img onClick={()=>setshowsearch(true)} src={assets.search_icon} alt="search_icon" className='w-5 cursor-pointer'></img>
-          <div >
-          <link to='/login'><img src={assets.profile_icon} alt="profile_icon" className='w-5 cursor-pointer'></img></link>
+          <div className='group relative'>
+          <Link to='/login'><img src={assets.profile_icon} alt="profile_icon" className='w-5 cursor-pointer'></img></Link>
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
           <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500'>
             <p className='cursor-pointer hover:text-black'>My Profile</p>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  const[currentstate,setcurrentstate]=useState("Sign Up");
+
   return (
-    <div></div>
+    <form className='flex flex-col items-center  max-w-96  m-auto mt-14 mb-60 gap-4 text-gray-800'>
+      <div className='inline-flex  items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currentstate} </p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
+      </div>
+     
+     
+      {currentstate === "login" ? "" :<input type='text' className='w-full px-3 py-2 border border-gray-800 ' placeholder='Name' required />} 
+       <input type='email' className='w-full px-3 py-2 border border-gray-800 ' placeholder='Email' required/> 
+       <input type='password' className='w-full px-3 py-2 border border-gray-800 ' placeholder='Password' required/> 
+       <div className=' w-full flex justify-between mt-[-8px'>
+          <p className='cursor-pointer'>Forget password</p>
+          {
+            currentstate === 'Sign Up' ? <p onClick={()=>setcurrentstate("login")} className='cursor-pointer' >login</p> : <p onClick={()=>setcurrentstate("Sign Up")} className='cursor-pointer' >Create Account</p>
+          }
+       </div>
+    </form>
   )
 }
-
+ 
 export default Login
